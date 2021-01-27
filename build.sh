c6619666ac24941eeea7d1543f31ae320819c40c
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-27 11:43:50 +0100

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
+#!/bin/bash
 docker build --no-cache -t hackinglab/alpine-python-flask:3.2.0 -t hackinglab/alpine-python-flask:3.2 -t hackinglab/alpine-python-flask:latest -f Dockerfile .
-
-docker push hackinglab/alpine-python-flask
-docker push hackinglab/alpine-python-flask:3.2
-docker push hackinglab/alpine-python-flask:3.2.0
-
 
